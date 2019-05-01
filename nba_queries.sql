7aeb0d514c85ff9e2f81804e3a1b8fe47224b2ab
Sidgi Samanduyev <samanduyev@gmail.com>
2019-05-01 00:55:37 -0400

    completed with all bonuses

--- nba_queries.sql
+++ nba_queries.sql
@@ -50,11 +50,15 @@ GROUP BY team;
 -- [7] The average age for all players who played more than 40 games.
 -- result set: [average_age]
 
-SELECT team,AVG(age) AS average_age
+-- SELECT team,AVG(age) AS average_age
+-- FROM players
+-- WHERE games>40
+-- GROUP BY team;
+-- ORDER BY average_age DESC;
+
+SELECT AVG(age) AS average_age
 FROM players
-WHERE games>40
---GROUP BY team
---ORDER BY average_age DESC;
+WHERE games>40;
 
 --------------------------------------------
 -- BONUS - Advanced SQL Queries
@@ -66,23 +70,39 @@ WHERE games>40
 -- HINT: GROUP BY
 -- result set: [team,total_points]
 
-
-
+SELECT team, SUM(points) AS total_points
+FROM players
+GROUP BY team 
+ORDER BY total_points DESC;
 -- [9] The age and the average points per game for that age, ordered from oldest to
 -- youngest for all ages.
 -- result set: [age, ppg]
 
-
+SELECT age, AVG(points/games) AS ppg 
+FROM players
+GROUP BY age
+ORDER BY age DESC;
 
 -- [10] The team and the the number of players who score above 12 points per game on
 --- that team, ordered from the most number of players to the least number of players.
 -- result set: [team, player_count]
 
-
+SELECT team, COUNT(name) AS player_count
+FROM players
+WHERE points/games > 12
+GROUP BY team 
+ORDER BY player_count DESC;
 
 -- SUPER BONUS !!
 -- [11] take the above query and only show those teams who are HAVING more than 3 players in each group.
 -- result set: [team, player_count]
 
+SELECT team, 
+COUNT(name) AS player_count
+FROM players
+WHERE points/games > 12  
+GROUP BY team
+HAVING COUNT(name)>3 
+ORDER BY player_count ASC;
 
 
